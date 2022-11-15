d14b077d8d3ed2551fc1565cfef8a3fda7366ff5
Ola Ishola <olaishola@hotmail.co.uk>
2022-11-15 19:50:28 +0100

    create .env file to hide db params & install nestjs/config pkg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ProductsModule,
     MongooseModule.forRoot(
-      'mongodb+srv://olaish:zYdjefJJOl75AC1A@api-db.ofcj2.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@api-db.ofcj2.mongodb.net/nestjs-demo?retryWrites=true&w=majority`,
     ),
   ],
   controllers: [AppController],
